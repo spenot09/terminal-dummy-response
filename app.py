939e8cf8cf04f6f229ec5114909012d6245241e4
Spencer Collins <you@example.com>
2023-06-23 18:19:30 +0100

    accept post

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=('GET', 'POST'))
 def home():
     """
     Reroute
     """
-   
 
     return redirect(url_for('create'))
+
 
 @app.route('/chatgpt', methods=('GET', 'POST'))
 def create():
@@ -18,6 +19,6 @@ def create():
     Example function where ChatGPT response will come
     """
     if request.method == 'POST':
-        return request.data+" - Good question, sadly I'm not fully functional juuuuust yet"
+        return request.get_json()["prompt"]+" - Good question, sadly I'm not fully functional juuuuust yet"
     if request.method == 'GET':
         return "Try asking me a question :)"
